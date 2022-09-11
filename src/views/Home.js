b61da34537f5fd8ebd733328b9560acd1e3d0d4b
oluwatobisimi <bamitobs@gmail.com>
2022-09-11 21:20:16 +0100

    full website 3rd commit

--- src/views/Home.js
+++ src/views/Home.js
@@ -11,20 +11,20 @@ import MobileNavBar from '../components/MobileNavBar'
 const Home = () => {
   return (
     <>
-    <MobileNavBar/>
-     <div className="App bg-[#FCF9F7] md:h-screen lg:h-auto space-y-10 md:space-y-16 lg:space-y-20 text-[#2B1105] w-screen  2xl">
-      <Hero/>
-      <NavBar/>
-      <Quote/>
-      <Sections/>
-      <div className="my-20 w-screen aspect-video">
-      <iframe className="w-full aspect-video" src="https://www.youtube.com/embed/UMJVXRK3NPk" title="Meet The Unconventionals" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
-      <Quote2/>
-      {/* <SaveHero/>
+      <MobileNavBar />
+      <div className="App bg-[#FCF9F7] md:h-screen lg:h-auto space-y-10 md:space-y-16 lg:space-y-20 text-[#2B1105] w-screen  2xl">
+        <Hero />
+        <NavBar />
+        <Quote />
+        <Sections />
+        <div className="my-20 w-screen aspect-video">
+          <iframe className="w-full aspect-video" src="" title="We're The Unconventionals!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <Quote2 />
+        {/* <SaveHero/>
       <SaveDateBody/> */}
-      <Footer/>
-    </div>
+        <Footer />
+      </div>
     </>
   )
 }
